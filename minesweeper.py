83f4242fce9941e89281dbd5920b3218e02cf154
jesusFreak1 <32850733+kkellogg378@users.noreply.github.com>
2024-02-11 16:16:30 -0500

    Added locking/flagging functionality, updated assets

--- minesweeper.py
+++ minesweeper.py
@@ -23,18 +23,37 @@ safe5 = tk.PhotoImage(file = './assets/5.png')
 safe6 = tk.PhotoImage(file = './assets/6.png')
 safe7 = tk.PhotoImage(file = './assets/7.png')
 safe8 = tk.PhotoImage(file = './assets/8.png')
+bomb = tk.PhotoImage(file = './assets/bomb.png')
+bomb_red = tk.PhotoImage(file = './assets/bomb_red.png')
+flag = tk.PhotoImage(file = './assets/flag.png')
 
 # Define board grid
-height = 15 # i
-width = 15  # j
-grid = [[0]*width]*height
+height = 15 # i'th row
+width = 15  # j'th column
+grid = [[0]*width for _ in range(height)]
 k = 0 # was getting issues with first variable of lambda without this
 
-def left(i, j):
-    print('clicked left', i, j)
+# Define 2D array for locking squares
+locked = [[False]*width for _ in range(height)]
+for i in range(0,height):
+    for j in range(0,width):
+        locked[i][j] = False
 
+# Function for left-clicking a button
+def left(i, j):
+    if (locked[i][j] == True): # check if square is locked
+        return
+    print('clicked left', i, j) # debug
+
+# Function for right-clicking a button
 def right(i, j):
-    print('clicked right', i, j)
+    if (locked[i][j] == True):
+        grid[i][j].config(image = empty)
+        locked[i][j] = False
+    else:
+        grid[i][j].config(image = flag)
+        locked[i][j] = True
+    print('locked', i, j) # debug
 
 # Generate board
 for i in range(0, height):
